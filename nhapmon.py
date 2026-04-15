e073f1acba0013770796080f540fcaec0ac30ba0
namtv-2910 <toanhocabc29@gmail.com>
2026-04-15 08:10:48 +0700

    Add files via upload

---
+++ nhapmon.py
@@ -0,0 +1,22 @@
+ # print("Hello")
+# ten = "Y"
+# tuoi = 20
+# don_gia = 123.45
+# PhaiNu = True
+# x, y, z = 1, 2, 3
+# print('(%d * %d) + %d = %d' % (x, y, z, (x*y)+z))
+# print("Xin chào " + ten)
+print("hình a")
+print('*'*5)
+print('*'*5)
+print('*'*5)
+
+
+print("hình b")
+print('*'*5)
+print('*'+' '*3+'*')
+print('*'*5)
+
+
+
+print("QUE HUONG\n\n...\n\nQuê hương là chùm khế ngọt\n\tCho con trèo hái mỗi ngày\n\t\tQuê hương là đường đi học\n\t\t\tCon về rợp bướm vàng bay\n\n...\n\n\t\t\t\t\tĐỗ Trung Quân")
