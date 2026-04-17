d127b423f9a1bc563febdc2a08e3a3e88c063859
Van Nam <toanhocabc29@gmail.com>
2026-04-17 11:53:09 +0700

    add bt control statements and loop

--- buoi1_loop.py
+++ buoi1_loop.py
@@ -1,42 +1,46 @@
-# n = int(input("Nhập n: "))
-# c = 0
-# l = 0
-# while n > 0:
-#     tmp = n % 10
-#     if tmp % 2 == 0:
-#         c += 1
-#     else:
-#         l += 1
-#     n //= 10
-# print("Số lượng chữ số chẵn:", c)
-# print("Số lượng chữ số lẻ:", l)
+# bai 4
+print("Bài 4")
+n = int(input("Nhập n: "))
+c = 0
+l = 0
+while n > 0:
+    tmp = n % 10
+    if tmp % 2 == 0:
+        c += 1
+    else:
+        l += 1
+    n //= 10
+print("Số lượng chữ số chẵn:", c)
+print("Số lượng chữ số lẻ:", l)
 
-# # bài 5
-# n1 = int(input("Nhập n: "))
-# s = 0
-# tich = 1
-# while n1:
-#     tmp = n1 % 10
-#     s += tmp
-#     tich *= tmp
-#     n1 //= 10
-# print("Tổng các chữ số:", s)
-# print("Tích các chữ số:", tich)
+# bài 5
+print("Bài 5")
+n1 = int(input("Nhập n: "))
+s = 0
+tich = 1
+while n1:
+    tmp = n1 % 10
+    s += tmp
+    tich *= tmp
+    n1 //= 10
+print("Tổng các chữ số:", s)
+print("Tích các chữ số:", tich)
 
-# # bai 6
-# n2 = int(input("Nhập n: "))
-# res = -1e9
-# while n2:
-#     tmp = n2 % 10
-#     if tmp > res:
-#         res = tmp
-#     n2 //= 10
-# print("Chữ số lớn nhất:", res)
+# bai 6
+print("Bài 6")
+n2 = int(input("Nhập n: "))
+res = -1e9
+while n2:
+    tmp = n2 % 10
+    if tmp > res:
+        res = tmp
+    n2 //= 10
+print("Chữ số lớn nhất:", res)
 
 #bai 7
-
+print("Bài 7")
 for i in range(0, 3):
-    n = int(input("Nhập số cần kt: "))
+    n = int(input("Nhập số cần kt thu {}: ".format(i + 1)))
     kt = True   # giả sử đúng trước
 
     while n > 0:
