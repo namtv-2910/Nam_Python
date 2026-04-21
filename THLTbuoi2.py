7f27120ece4ef1ecbf7b24fda5128afc02eac751
Van Nam <toanhocabc29@gmail.com>
2026-04-21 09:26:57 +0700

    updates bai 7 bai 13

---
+++ THLTbuoi2.py
@@ -0,0 +1,32 @@
+# #bài 6
+# s = input("Nhập một chuỗi: ")
+# tu = input("Nhập một từ: ")
+# print("Số từ {} trong chuỗi là: {}".format(tu, s.count(tu)))
+
+#bài 13
+s = """   Quê  hương
+Quê  hương  là  chùm khế  ngọt .
+   Cho  con trèo hái mỗi  ngày .
+Quê  hương là   đường  đi học .
+Con  về rợp  bướm vàng bay .
+   Đỗ   Trung Quân   """
+
+lines = s.split('\n')
+result = []
+
+for line in lines:
+    line = line.strip()          # bỏ khoảng trắng đầu/cuối
+    words = line.split()         # tách từ
+
+    new_line = ""
+    for word in words:
+        if word == "." or word == ",":
+            new_line = new_line.rstrip() + word   # dính dấu câu
+        else:
+            new_line += word + " "
+
+    result.append(new_line.strip())
+
+# in kết quả
+for line in result:
+    print(line)
