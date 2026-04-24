b1b12ed20a462af6667934cfac13b4c572e9b9fa
Van Nam <toanhocabc29@gmail.com>
2026-04-24 10:28:46 +0700

    Bai 2 Giai Thua

--- De_quy.py
+++ De_quy.py
@@ -7,4 +7,12 @@ def SumDigit(n):
 n = int(input("Nhập một số nguyên dương: "))
 print("Tổng các chữ số của", n, "là:", SumDigit(n))
 
+#bai 2
+def Gt(n):
+    if n == 0:
+        return 1
+    else:
+        return n * Gt(n - 1)
+n = int(input("Nhập một số nguyên dương: "))
+print("Giai thừa của", n, "là:", Gt(n))
 
